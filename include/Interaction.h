9d0aa3f00712c60be266b37bd21fa04934aea55f
Toliak <ibolosik@gmail.com>
2019-04-11 01:19:36 +0300

    Added few comments

--- include/Interaction.h
+++ include/Interaction.h
@@ -6,32 +6,41 @@
 #include "Database.h"
 #include "Interaction.h"
 
+// Описание класса Command, для использование в коде ниже
 class Command;
 
-struct Interaction
+/**
+ * @brief Синглтон класс, отвечающий за взаимодействие между пользователем и программой
+ */
+class Interaction
 {
 private:
-    static Interaction *instance;
-    Interaction() = default;
+    static Interaction *instance;       ///< Единственный объект данного типа
 
+    Interaction() = default;
     ~Interaction();
 
 protected:
-    Console console;
-    std::unordered_map<std::string, HybridDatabase> data = {};
-    HybridDatabase *currentDatabase = nullptr;
-
-    std::unordered_map<std::string, Command *> commands = {};
+    Console console;                                            ///< Консоль
+    std::unordered_map<std::string, HybridDatabase> data = {};  ///< Словарь (название, БД)
+    HybridDatabase *currentDatabase = nullptr;                  ///< Указатель на текущую базу данных
+    std::unordered_map<std::string, Command *> commands = {};   ///< Зарегистрированные команды
 
 public:
+    // Для синглтон класса удалены конструкторы копирования и перемещения
+
     Interaction(const Interaction &) = delete;
     Interaction(Interaction &&) = delete;
 
     Interaction &operator=(const Interaction &) = delete;
     Interaction &operator=(Interaction &&) = delete;
 
+    /**
+     * @return Экземляр класса
+     */
     static Interaction &getInstance()
     {
+        // Создает экземпляр, если он не был создан
         if (!Interaction::instance) {
             // Exists till the program closed
             Interaction::instance = new Interaction();
@@ -39,8 +48,19 @@ public:
         return *Interaction::instance;
     }
 
+    /**
+     * @brief Регистрирует команду
+     * @param name Название команды
+     * @param command Указатель на объект класса Command (либо наследованного от него класса)
+     * @return Статус
+     */
     bool registerCommand(const std::string &name, Command *command) noexcept;
 
+    /**
+     * @brief Выполняет команду
+     * @param pair Пара строк (команда, аргументы)
+     * @throw ExecuteInteractionException если команда не найдена
+     */
     void executeCommand(const std::pair<std::string, std::string> &pair);
 
     void setCurrentDatabase(HybridDatabase *interactionCurrentDatabase) noexcept
